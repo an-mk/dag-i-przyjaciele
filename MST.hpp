16f0eb6a6cef6d11f969c0e819eea234d89fbefb
Antoni Nowinowski <antoninowinowski@hotmail.com>
2019-04-28 19:43:04 +0200

    mst added to lab

--- MST.hpp
+++ MST.hpp
@@ -45,11 +45,52 @@ neighbourList mst(const neighbourList& g)
             priorityQueue.insert({top.to, (int)i, 1/*rand*/}); 
             //std::cout<<top.to<<" "<<i<<"\n";
         }
-        std::cout<<top.from<<" "<<top.to<<"\n";
+       // std::cout<<top.from<<" "<<top.to<<"\n";
         ret.addConnection(top.from, top.to);
         visited[top.to] = true;
     }
-    for (auto x : visited) std::cout<<x<<" ";
-    std::cout<<"\n";
+    //for (auto x : visited) std::cout<<x<<" ";
+    //std::cout<<"\n";
+    return ret;
+}
+
+adjacencyMatrix mst(const adjacencyMatrix& g)
+{
+    int toTake = g.getSize();
+    adjacencyMatrix ret(toTake);
+    std::vector<bool> visited(toTake);
+    std::set<prev, cmp> priorityQueue;
+    for (int i = 0; i < g.getSize(); i++)
+    {
+        if (!g.isConnected(0, i))
+            continue;
+        priorityQueue.insert({0, (int)i, 1/*rand*/});
+        //std::cout<<0<<" "<<i<<"\n";
+    }
+    while (toTake && !priorityQueue.empty())
+    {
+        toTake--;
+        auto top = *priorityQueue.begin();
+        priorityQueue.erase(priorityQueue.begin());
+        
+        while (visited[top.to] && !priorityQueue.empty())
+        {
+            top = *priorityQueue.begin();
+            priorityQueue.erase(priorityQueue.begin());
+        }
+        //if (priorityQueue.empty()) break;
+        for (int i = 0; i < g.getSize(); i++)
+        {
+            if (!g.isConnected(top.to, i))
+                continue;
+            priorityQueue.insert({top.to, (int)i, 1/*rand*/}); 
+            //std::cout<<top.to<<" "<<i<<"\n";
+        }
+       // std::cout<<top.from<<" "<<top.to<<"\n";
+        ret.addConnection(top.from, top.to);
+        visited[top.to] = true;
+    }
+    //for (auto x : visited) std::cout<<x<<" ";
+    //std::cout<<"\n";
     return ret;
 }
--- main.cpp
+++ main.cpp
@@ -13,7 +13,12 @@ int main()
 	mst(generateRandomDAG(15,1).second);
     MesurementLab<TestDataType, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600> lab;
     lab.setTestGenerator([](const size_t &sizeOfTest){ return generateRandomDAG(sizeOfTest, 0.6f); });
-    lab([](const std::pair<adjacencyMatrix, neighbourList>& G){ ts(G.first); }, "adjacencyMatrix");
-    lab([](const std::pair<adjacencyMatrix, neighbourList>& G){ ts(G.second); }, "neighbourList");
+    lab([](const std::pair<adjacencyMatrix, neighbourList>& G){ ts(G.first); }, "ts adjacencyMatrix");
+    lab([](const std::pair<adjacencyMatrix, neighbourList>& G){ ts(G.second); }, "ts neighbourList");
+
+	//MesurementLab<TestDataType, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600> lab;
+    //lab.setTestGenerator([](const size_t &sizeOfTest){ return generateRandomDAG(sizeOfTest, 0.6f); });
+    lab([](const std::pair<adjacencyMatrix, neighbourList>& G){ mst(G.first); }, "mst adjacencyMatrix");
+    lab([](const std::pair<adjacencyMatrix, neighbourList>& G){ mst(G.second); }, "mst neighbourList");
     return 0;
 }
